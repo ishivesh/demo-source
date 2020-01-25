1b0eb6c4c10584fb00409f29045346452b0a59ab
Shivesh Kumar <shivesh.kumar@prime-research.com>
2020-01-25 13:34:29 +0100

    Initial commit

---
+++ src/main/java/com/shiv/scdf/example/timesource/DemoSourceApplication.java
@@ -0,0 +1,30 @@
+package com.shiv.scdf.example.timesource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
+
+import java.time.Duration;
+import java.util.function.Supplier;
+
+@SpringBootApplication
+public class DemoSourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSourceApplication.class, args);
+    }
+
+    @Bean
+    public Supplier<Flux<String>> emit() {
+        return () ->
+            Flux.interval(Duration.ofSeconds(1))
+                .onBackpressureLatest()
+                .map(aLong -> aLong + " - demo")
+                .log();
+    }
+}
